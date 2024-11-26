e4631364c3c9251a57200b357c0466b3c94fea1e
SirJohnGabriel <j.g.pagtalunan14@gmail.com>
2024-11-26 16:00:13 +0800

    update

--- CreateAndMint.sol
+++ CreateAndMint.sol
@@ -1,5 +1,18 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract CreateAndMint {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CreateAndMint is ERC20 {
+    constructor() ERC20("JohnGabrielPagtalunan", "JGP") {
+        _mint(msg.sender, 100 * 10**uint(decimals()));
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
 }
